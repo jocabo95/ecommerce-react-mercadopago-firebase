ca76c43531f7d33adfefae88d1259696b48386c9
jose <camargojl1995@gmail.com>
2024-02-21 11:29:09 -0500

    auth context local storage done

--- src/components/context/AuthContext.jsx
+++ src/components/context/AuthContext.jsx
@@ -5,17 +5,23 @@ export const AuthContext = createContext()
 
 const AuthContextComponent = ({children}) => {
 
-    const [user, setUser] = useState({})
-    const [isLogged, setIsLogged] = useState(true)
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")) || {})
+    const [isLogged, setIsLogged] = useState(JSON.parse(localStorage.getItem("isLogged"))  || false)
 
     let handleLoginAuth = (userData) => {
         setUser(userData)
         setIsLogged(true)
+
+        localStorage.setItem("isLogged", JSON.stringify(true))        
+        localStorage.setItem("user", JSON.stringify(userData))
     }
 
     let handleLogoutAuth =() =>{
         setUser({})
         setIsLogged(false)
+
+        localStorage.removeItem("user")
+        localStorage.removeItem("isLogged")
     }
 
     const data = {handleLoginAuth, handleLogoutAuth, user, isLogged}
@@ -29,3 +35,5 @@ const AuthContextComponent = ({children}) => {
 }
 
 export default AuthContextComponent
+
+
