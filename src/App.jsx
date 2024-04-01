77b88b6a795b2a7eb30f6a70f5e35937d0f07539
jose <camargojl1995@gmail.com>
2024-04-01 11:56:09 -0500

    set up theme object that works

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,41 @@ import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./router/AppRouter";
 import CartContextComponent from "./components/context/CartContext";
 import AuthContextComponent from "./components/context/AuthContext";
+import { createTheme, ThemeProvider } from "@mui/material";
+
 
 function App() {
+  
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#B8A9A0",
+        light: "#F5E5C8",
+        dark: "#9E6E51",
+        contrastText: "#36454F",
+      },
+      secondary: {
+        main: "#36454F",
+      },
+      background: {
+        main: "#E3D3C8",
+        light: "#ffffff",
+        dark: "#A06348",
+      },
+    },
+  });
 
   return (
-    <BrowserRouter>
-      <CartContextComponent>
-        <AuthContextComponent>
-         <AppRouter />
-        </AuthContextComponent>
-      </CartContextComponent>
-    </BrowserRouter>
+    // theme prodivder helps customize pallete
+    <ThemeProvider theme={theme}> 
+      <BrowserRouter>
+        <CartContextComponent>
+          <AuthContextComponent>
+          <AppRouter />
+          </AuthContextComponent>
+        </CartContextComponent>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
