a5ae7cb9c41eb5c8e9b7b0ff5ebf5e806f32772d
jose <camargojl1995@gmail.com>
2024-05-23 17:56:08 -0500

    mostly done with style

--- src/components/pages/checkout/checkoutForm/CheckoutForm.jsx
+++ src/components/pages/checkout/checkoutForm/CheckoutForm.jsx
@@ -1,4 +1,12 @@
-import { Box, Button, Grid, Paper, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Grid,
+  MenuItem,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { useContext } from "react";
 import { CartContext } from "../../../context/CartContext";
 
@@ -7,6 +15,16 @@ const CheckoutForm = ({ data }) => {
 
   const { handleChange, handleBuy } = data;
 
+  const clientData = [
+    { name: "nombre", example: "Pedro" },
+    { name: "apeliido", example: "González" },
+    { name: "teléfono", example: "312 834 209|" },
+    { name: "correo", example: "pedro.gonzalez@gmail.com" },
+    { name: "verificar correo", example: "pedro.gonzalez@gmail.com" },
+  ];
+
+  const ciudades = ["Bogotá", "Medellin", "Cartagena", "Otras"];
+
   console.log("Cart: ", cart);
 
   return (
@@ -14,10 +32,15 @@ const CheckoutForm = ({ data }) => {
       {/* ORDER SUMMARY */}
       <Grid item xs={12} md={5}>
         <Paper
-          sx={{ m: { xs: "1rem", md: "3rem", height: "100%" }, p: "1rem" }}
+          elevation={4}
+          sx={{
+            m: { xs: "1rem", md: "3rem" },
+            p: "1rem",
+            height: { xs: "auto", md: "100%" },
+          }}
         >
           <p>Total</p>
-          <Typography variant="h4" sx={{ mb: "1rem", fontWeight: "500" }}>
+          <Typography variant="h4" sx={{ mb: "1.5rem", fontWeight: "500" }}>
             $ {Intl.NumberFormat().format(getTotalPrice())}
           </Typography>
 
@@ -27,9 +50,7 @@ const CheckoutForm = ({ data }) => {
               return (
                 <Grid container key={prod.id} sx={{ mb: "1rem" }}>
                   <Grid item xs={8}>
-                    <Box>
-                      <Typography>{prod.title}</Typography>
-                    </Box>
+                    <Typography>{prod.title}</Typography>
                   </Grid>
                   <Grid item xs={4}>
                     <Typography>
@@ -39,6 +60,18 @@ const CheckoutForm = ({ data }) => {
                 </Grid>
               );
             })}
+
+            {/* costo envio */}
+            <Grid container sx={{ mb: "1rem" }}>
+              <Grid item xs={8}>
+                <Typography>Costo de envío</Typography>
+              </Grid>
+              <Grid item xs={4}>
+                <Typography>
+                  $ {Intl.NumberFormat().format(3000)}
+                </Typography>
+              </Grid>
+            </Grid>
           </Box>
         </Paper>
       </Grid>
@@ -49,12 +82,41 @@ const CheckoutForm = ({ data }) => {
         xs={12}
         md={6}
         sx={{
-          mr: { xs: "1rem", md: "3rem", height: "100%" },
+          mx: { xs: "1rem", md: "2rem", height: "100%" },
           mt: { xs: "1rem", md: "3rem", height: "100%" },
-          p: "1rem",
         }}
       >
+        {clientData.map((el) => {
+          return (
+            <Box key={el}>
+              <Typography sx={{ fontWeight: "600" }}>{el.name}</Typography>
+              <TextField
+                name={el.name}
+                variant="outlined"
+                size="small"
+                fullWidth
+                onChange={handleChange}
+                sx={{ mb: "1rem", mr: "1rem" }}
+              />
+            </Box>
+          );
+        })}
+
+        <Typography sx={{ fontWeight: "600" }}>Ciudad</Typography>
         <TextField
+          id="filled-select-currency"
+          select
+          label="Select"
+          variant="outlined"
+          fullWidth
+        >
+          {ciudades.map((option) => (
+            <MenuItem key={option} value={option}>
+              {option}
+            </MenuItem>
+          ))}
+        </TextField>
+        {/* <TextField
           name="cp"
           variant="outlined"
           label="codigo postal"
@@ -65,8 +127,20 @@ const CheckoutForm = ({ data }) => {
           variant="outlined"
           label="telefono"
           onChange={handleChange}
-        />
-        <Button onClick={handleBuy}>Mercado pago</Button>
+        /> */}
+        <Button
+          onClick={handleBuy}
+          variant="contained"
+          color="details"
+          sx={{
+            mt: "3rem",
+            opacity: "0.9",
+            color: "white",
+            width: "100%",
+          }}
+        >
+          Pagar
+        </Button>
       </Grid>
     </Grid>
   );
